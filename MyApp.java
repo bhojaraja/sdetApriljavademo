d1a52e70068f286a78becc96856ba89061ebd873
Lab User <labuser@ip-172-31-0-25.ap-south-1.compute.internal>
2023-04-26 11:18:33 +0000

    next statement is addedd

--- MyApp.java
+++ MyApp.java
@@ -4,6 +4,7 @@ class MyApp
 public static void main(String str[])
 {
 System.out.println("Hello All, This is Jenkin Tool Demontration example");
+System.out.println("This example is Not making use of Maven");
 }
 
 }
